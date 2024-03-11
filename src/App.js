9d2573a601255fc1d98f60e7782c157484d16d15
bon4i <bonchigam@gmail.com>
2024-03-11 23:01:05 +0300

    Add styles and state

--- src/App.js
+++ src/App.js
@@ -1,41 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+import styles from './App.module.css';
+import React, { useState } from 'react';
 
 export const App = () => {
-    return React.createElement(
-        'div',
-        {
-            className: 'App',
-        },
-        React.createElement(
-            'header',
-            {
-                className: 'App-header',
-            },
-            React.createElement('img', {
-                src: logo,
-                className: 'App-logo',
-                alt: 'logo',
-            }),
-            React.createElement(
-                'p',
-                null,
-                'Edit ',
-                React.createElement('code', null, 'src/App.js'),
-                ' and save to reload.',
-            ),
-            React.createElement(
-                'a',
-                {
-                    className: 'App-link',
-                    href: 'https://reactjs.org',
-                    target: '_blank',
-                    rel: 'noopener noreferrer',
-                },
-                'Learn React',
-            ),
-            React.createElement('p', null, new Date().getFullYear()),
-        ),
+    const [operand1, setOperand1] = useState('');
+    const [operand2, setOperand2] = useState(0);
+    const [operator, setOperator] = useState('');
+
+    const NUMS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    const OPERATORS = ['+', '-', '=', 'C'];
+
+    const handleClickCancel = () => {
+        setOperand1('');
+        setOperand2('');
+        setOperator('');
+    };
+    const numsButtons = NUMS.map((num, index, item) => (
+        <div
+            className={styles['button-number']}
+            key={item}
+            onClick={() => setOperand1((prevNum) => prevNum + num)}
+        >
+            {num}
+        </div>
+    ));
+
+    const operatorsButtons = OPERATORS.map((operator, index, item) => (
+        <div
+            className={styles['button-operator']}
+            key={item}
+            onClick={() => setOperator(operator)}
+        >
+            {operator}
+        </div>
+    ));
+
+    return (
+        <div className={styles.form}>
+            <div className={styles['dispalay-container']}>
+                <p>
+                    {operand1 || 'Ваши числа'} {operator}
+                </p>
+                <p>Здесь будет результат</p>
+            </div>
+            <div className={styles['buttons-container-operators']}>
+                {operatorsButtons}
+            </div>
+            <div className={styles['buttons-container-numbers']}>{numsButtons}</div>
+        </div>
     );
 };
